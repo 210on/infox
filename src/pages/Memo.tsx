92b61c2f2b049dbe04f5b60a01836814ff69a00b
kirk1027 <kirk2015h@yahoo.co.jp>
2023-12-08 17:50:07 +0900

    色の変更

--- src/pages/Memo.tsx
+++ src/pages/Memo.tsx
@@ -1,5 +1,5 @@
 import ReactMarkdown from 'react-markdown';
-import { Box, Button, Grid, TextField, Typography } from "@mui/material";
+import { Box, Button, Grid, MenuItem, Select, SelectChangeEvent, TextField, Typography } from "@mui/material";
 import { userAtom } from "../states/userAtom";
 import { useRecoilState, useSetRecoilState } from "recoil";
 import { saveMemo } from "../services/saveMemo";
@@ -106,7 +106,18 @@ export function Memo(): JSX.Element {
                 fullWidth
                 value={content}
                 onChange={(e) => setContent(e.target.value)}
+                InputProps={{ style: { color: textColor } }}
               />
+            </Grid>
+            <Grid item xs={12}>
+              {/* ドロップダウンメニュー */}
+              <Select value={textColor} onChange={handleTextColorChange}>
+                <MenuItem value="black">Black</MenuItem>
+                <MenuItem value="red">Red</MenuItem>
+                <MenuItem value="green">Green</MenuItem>
+                <MenuItem value="blue">Blue</MenuItem>
+                {/* 他の色の選択肢を追加できます */}
+              </Select>
             </Grid>
             <Grid item xs={12}>
               <Button variant="contained" onClick={() => save()}>
