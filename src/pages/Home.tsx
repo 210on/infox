3a53698dc2c08e335e2a0c8fab632967f170d518
ukisu369 <kouto.ukisu@gmail.com>
2023-12-13 13:16:02 +0900

    test uki

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -39,6 +39,7 @@ export function Home(): JSX.Element {
         これは自身の経験・知識を簡単に集積・体系化ができ、<br />
         その他のメモ帳ツールとは違って、 <br />
         シンプルで使いやすいUIと体系化機能が備わっています。
+        aaaa
       </Typography>
       <Box
         sx={{
