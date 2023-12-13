085532d8aae79ea0a9b9a7df71837ec349016553
ukisu369 <kouto.ukisu@gmail.com>
2023-12-13 14:06:38 +0900

    検索機能の追加

--- src/pages/MemoList.tsx
+++ src/pages/MemoList.tsx
@@ -5,6 +5,7 @@ import {
   ListItem,
   ListItemText,
   Typography,
+  TextField,
   Select,
   MenuItem,
   SelectChangeEvent,
@@ -23,6 +24,8 @@ import { messageAtom } from "../states/messageAtom";
 import { SimpleDialog } from "../components/SimpleDialog";
 import { exceptionMessage, successMessage } from "../utils/messages";
 
+
+
 export function MemoList(): JSX.Element {
   const [loginUser] = useRecoilState(userAtom);
   const setMessageAtom = useSetRecoilState(messageAtom);
@@ -33,6 +36,7 @@ export function MemoList(): JSX.Element {
   const [defaultOrder, setDefaultOrder] = useState<Memo[]>([]);
   const [orderBy, setOrderBy] = useState("default");
   const [reverseOrder, setReverseOrder] = useState(false); // 逆順フラグ
+  const [searchKeyword, setSearchKeyword] = useState<string>("");
 
   const moveToMemo = (id?: string) => {
     if (id) {
@@ -104,6 +108,29 @@ export function MemoList(): JSX.Element {
     setMemoList((prevList) => [...prevList].reverse());
   };
 
+  const searchMemos = (keyword: string) => {
+    if (keyword.trim() === "") {
+      // 検索キーワードが空の場合はデフォルトのメモリストを表示
+      setMemoList([...defaultOrder]);
+    } else {
+      // メモリストからキーワードにマッチするものをフィルタリングして表示
+      const filteredMemos = memoList.filter(
+        (memo) =>
+          memo.title.toLowerCase().includes(keyword.toLowerCase()) ||
+          memo.content.toLowerCase().includes(keyword.toLowerCase())
+      );
+      setMemoList([...filteredMemos]);
+    }
+  };
+
+  const handleSearch = () => {
+    searchMemos(searchKeyword);
+  };
+
+  const handleSearchInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchKeyword(event.target.value);
+  };
+
   return (
     <>
       <Typography variant="h2">Infox</Typography>
@@ -134,7 +161,20 @@ export function MemoList(): JSX.Element {
             label="Reverse"
             labelPlacement="start"
           />
+            
           </Box>
+          
+          <Box sx={{ marginTop: '20px' }}>
+          <TextField
+          label="Search memos"
+          value={searchKeyword}
+          onChange={handleSearchInputChange}
+        />
+        <Button variant="contained" onClick={handleSearch}>
+          Search
+        </Button>
+        </Box>
+
           <Button variant="contained" onClick={handleNewMemo}>
             New memo
           </Button>
